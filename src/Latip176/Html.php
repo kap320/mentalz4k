148f8821729771b1f63577ef299b1fc155a10db6
shanachan <77118749+kap320@users.noreply.github.com>
2024-01-12 20:40:23 +0700

    Update Html.php

--- src/Latip176/Html.php
+++ src/Latip176/Html.php
@@ -10,7 +10,7 @@ class Html {
                     <ul class="list">
                         <li><a href="/">Home</a></li>
                         <li><a href="/ongoing">Ongoing</a></li>
-                        <li><a href="https://wa.me/6283870396203">Contact Me</a></li>
+                        <li><a href="https://tiktok.com/@mentalz4k">Contact Me</a></li>
                         <li>
                             <form action="/search" method="get">
                                 <input type="text" name="keyword" class="keyword" placeholder="cari anime favorite kamu"> <button type="submit">Search</button>
@@ -31,14 +31,14 @@ class Html {
                             <div></div>
                         </li>
                     </ul>
-                    <span class="nav-brand" style="font-size: 20px;"><span style="color: #00f; text-shadow: 0 0 10px #00f, 0 0 20px #00f, 0 0 30px #00f;">Latip</span>Nime</span>
+                    <span class="nav-brand" style="font-size: 20px;"><span style="color: #00f; text-shadow: 0 0 10px #00f, 0 0 20px #00f, 0 0 30px #00f;">M</span>entalz4k</span>
                 </div>
             </div>
             <div class="navbar-content">
                 <ul>
                     <li id="item"><a href="/">Home</a></li>
                     <li id="item"><a href="/ongoing">Ongoing</a></li>
-                    <li id="item"><a href="https://wa.me/6283870396203">Contact Me</a></li>
+                    <li id="item"><a href="https://tiktok.com/@mentalz4k">Contact Me</a></li>
                     <li id="item">
                         <form action="/search" method="get">
                             <input type="text" name="keyword" class="keyword" placeholder="cari anime favorite kamu"> <button type="submit">Search</button>
@@ -53,7 +53,7 @@ class Html {
         return <<<EOD
             <div class="footer">
                 <div style="width: 100%; height: 100px; text-align: center; ">
-                    <p class="text-light fw-bold" style="line-height: 100px;">&copy; 2023 - 2024 Made By ❤️ <a href="https://latipharkat.my.id/" class="latip">Latip176</a></p>
+                    <p class="text-light fw-bold" style="line-height: 100px;">&copy; 2024 Made By ❤️ <a href="https://tiktok.com/@mentalz4k/" class="Mentalz4k">Rendy</a></p>
                 </div>
             </div>
         EOD;
